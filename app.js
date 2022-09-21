41f5b5668a2317523c414ec93474d4fdc0bb3abb
Hannah <hannah.marie.senft@gmail.com>
2022-09-21 18:50:24 -0400

    post ctrl changes

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use((req, res, next) => {
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/posts', postRoutes);
-app.use('/api/auth', userRoutes);
+app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
 // app.use('/api/comments', commentRoutes);
 
 module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// const auth = require('../middleware/auth');
-// const signupValidation = require('../middleware/validation/signupValidation');
+const auth = require('../middleware/auth');
+const signupValidation = require('../middleware/validation/signupValidation');
 
 const userCtrl = require('../controllers/userController');
 
 // routes
-// router.post('/login', auth, userCtrl.login);
-// router.post('/signup', function(req, res) {
-//     auth, signupValidation, userCtrl.signup
-// });
+router.post('/login', auth, userCtrl.login);
+router.post('/signup', function(req, res) {
+    auth, signupValidation, userCtrl.signup
+});
 
 // postman routes
-router.post('/login', userCtrl.login);
-router.post('/signup', userCtrl.signup);
+// router.post('/login', userCtrl.login);
+// router.post('/signup', userCtrl.signup);
 
 module.exports = router;
