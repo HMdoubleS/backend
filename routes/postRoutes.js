3bead65abc4a092723ebe8d4842f59d34845014a
Hannah <hannah.marie.senft@gmail.com>
2022-10-17 19:53:51 -0400

    fixing post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-// const auth = require('../middleware/auth');
-// const multer = require('../middleware/multer-config');
-// const postValidation = require('../middleware/validation/postValidation');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const postValidation = require('../middleware/validation/postValidation');
 
 const postCtrl = require('../controllers/postController');
 
 // routes
 // router.get('/',  function(req, res) { auth, postCtrl.getAllPosts });
-router.get('/', postCtrl.getAllPosts);
 // router.post('/',  function(req, res) { auth, multer, postValidation, postCtrl.addPost });
 // router.get('/:id', function(req, res) { auth, postCtrl.getOnePost });
 // router.put('/:id', function(req, res) { auth, multer, postValidation, postCtrl.modifyPost });
 // router.delete('/:id', function(req, res) { auth, postCtrl.deletePost });
 
 // postman routes
-// router.get('/', postCtrl.getAllPosts); 
-// router.post('/', multer, postCtrl.addPost);
-// router.get('/:id', postCtrl.getOnePost); 
-// router.put('/:id', multer, postCtrl.modifyPost);
-// router.delete('/:id', postCtrl.deletePost);
+router.get('/', postCtrl.getAllPosts); 
+router.post('/', multer, postCtrl.addPost);
+router.get('/:id', postCtrl.getOnePost); 
+router.put('/:id', multer, postCtrl.modifyPost);
+router.delete('/:id', postCtrl.deletePost);
 
 module.exports = router;
